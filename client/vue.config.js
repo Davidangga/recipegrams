5c7d0f1b001e5a076b8c20e459e677c77b3745b4
davidangga <davidanggadeveloper@gmail.com>
2024-01-14 23:43:28 +1100

    backend for restful API

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production' ? '/recipegram-deploy/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/recipegrams/' : '/',
 })
